584383822dae5ddd3e69c7d51cce660e08e91dbe
fontilan <68748054+fontilan@users.noreply.github.com>
2022-09-27 15:27:47 +0200

    style: single quotes

--- script.js
+++ script.js
@@ -3,29 +3,29 @@ let playerChoice;
 let aiChoice;
 let playerScore = 0;
 let aiScore = 0;
-const playerChoiceEl = document.getElementById("player-choice");
-const aiChoiceEl = document.getElementById("ai-choice");
-const winOrLossEl = document.getElementById("win-or-loss");
-const playerScoreEl = document.getElementById("player-score");
-const aiScoreEl = document.getElementById("ai-score");
+const playerChoiceEl = document.getElementById('player-choice');
+const aiChoiceEl = document.getElementById('ai-choice');
+const winOrLossEl = document.getElementById('win-or-loss');
+const playerScoreEl = document.getElementById('player-score');
+const aiScoreEl = document.getElementById('ai-score');
 
 function rock() {
-  playerChoice = "rock";
-  playerChoiceEl.textContent = "👊";
+  playerChoice = 'rock';
+  playerChoiceEl.textContent = '👊';
   randomHand();
   winOrLoss();
 }
 
 function paper() {
-  playerChoice = "paper";
-  playerChoiceEl.textContent = "✋";
+  playerChoice = 'paper';
+  playerChoiceEl.textContent = '✋';
   randomHand();
   winOrLoss();
 }
 
 function scissors() {
-  playerChoice = "scissors";
-  playerChoiceEl.textContent = "✌️";
+  playerChoice = 'scissors';
+  playerChoiceEl.textContent = '✌️';
   randomHand();
   winOrLoss();
 }
@@ -33,42 +33,42 @@ function scissors() {
 function randomHand() {
   hand = Math.floor(Math.random() * 3);
   if (hand === 0) {
-    aiChoice = "rock";
-    aiChoiceEl.textContent = "👊";
+    aiChoice = 'rock';
+    aiChoiceEl.textContent = '👊';
   } else if (hand === 1) {
-    aiChoice = "paper";
-    aiChoiceEl.textContent = "✋";
+    aiChoice = 'paper';
+    aiChoiceEl.textContent = '✋';
   } else {
-    aiChoice = "scissors";
-    aiChoiceEl.textContent = "✌️";
+    aiChoice = 'scissors';
+    aiChoiceEl.textContent = '✌️';
   }
 }
 
 function winOrLoss() {
   if (playerChoice === aiChoice) {
     winOrLossEl.textContent = "IT'S A DRAW 😲";
-  } else if (playerChoice === "rock") {
-    if (aiChoice === "paper") {
-      winOrLossEl.textContent = "YOU LOSE 😭";
+  } else if (playerChoice === 'rock') {
+    if (aiChoice === 'paper') {
+      winOrLossEl.textContent = 'YOU LOSE 😭';
       aiScore += 1;
     } else {
-      winOrLossEl.textContent = "YOU WIN 😀";
+      winOrLossEl.textContent = 'YOU WIN 😀';
       playerScore += 1;
     }
-  } else if (playerChoice === "paper") {
-    if (aiChoice === "scissors") {
-      winOrLossEl.textContent = "YOU LOSE 😭";
+  } else if (playerChoice === 'paper') {
+    if (aiChoice === 'scissors') {
+      winOrLossEl.textContent = 'YOU LOSE 😭';
       aiScore += 1;
     } else {
-      winOrLossEl.textContent = "YOU WIN 😀";
+      winOrLossEl.textContent = 'YOU WIN 😀';
       playerScore += 1;
     }
-  } else if (playerChoice === "scissors") {
-    if (aiChoice === "rock") {
-      winOrLossEl.textContent = "YOU LOSE 😭";
+  } else if (playerChoice === 'scissors') {
+    if (aiChoice === 'rock') {
+      winOrLossEl.textContent = 'YOU LOSE 😭';
       aiScore += 1;
     } else {
-      winOrLossEl.textContent = "YOU WIN 😀";
+      winOrLossEl.textContent = 'YOU WIN 😀';
       playerScore += 1;
     }
   }
